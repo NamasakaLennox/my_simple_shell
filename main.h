cdecf626fa303a768e5ac09225bb94aeffec9fdc
NamasakaLennox <namasakalennox254@gmail.com>
2023-04-13 18:11:16 +0300

    [new]: performs basic operations given location

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+#endif
